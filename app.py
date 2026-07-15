56d74e21dfc9ba98a9571325261e108fffbc38f6
HamzaButt22 <hb979067@gmail.com>
2026-07-15 16:58:42 +0500

    Extended the system to handle multiple records using lists and dictionaries.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-def student(name, student_id, gpa):
-    print("\nStudent Record System!")
-    print("Name:", name)
-    print("ID:", student_id)
-    print("GPA:", gpa)
+student_database = []
 
-name = input("Enter Student Name: ")
+def Store_Records(name, student_id, gpa):
+    store_records = {
+        "Name": name,
+        "ID": student_id,
+        "GPA": gpa
+    }
+    student_database.append(store_records)
 
-student_id = int(input("Enter Student ID: "))
+def Display_All_Records():
+    print("\nStudent Record System!\n")
+    for student in student_database:
+        print("Name:", student["Name"])
+        print("ID:", student["ID"])
+        print("GPA:", student["GPA"])
+        print("-------------------------\n")
 
-gpa = float(input("Enter Student GPA: "))
+Store_Records("John Doe", 12345, 3.5)
+Store_Records("Jane Smith", 67890, 3.8)
 
-student(name, student_id, gpa)
+Display_All_Records()
